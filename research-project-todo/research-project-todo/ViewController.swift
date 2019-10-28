d7efdb68891c1f71aefc2d9e9629cba82f350f61
Oliver Rudzinski <oliver@Oliver-Rudzinski-MacBook-Pro.local>
2019-10-28 16:44:19 +0100

    Add empty Xcode project

---
+++ research-project-todo/research-project-todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  research-project-todo
+//
+//  Created by Oliver Rudzinski on 28.10.19.
+//  Copyright © 2019 Oliver Rudzinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
